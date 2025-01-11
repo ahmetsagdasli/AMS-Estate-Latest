e9255c57836e8d87c4b11d989a9e5cb881fcf8f6
ahmetsagdasli <ahmetssagdasli@gmail.com>
2025-01-12 01:28:09 +0300

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // Moved to the schema options
+  }
+);
+
+const User= mongoose.model('User', userSchema);
+
+export default User;
+
